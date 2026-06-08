61d765a8028825722187757a4b963f30566994fd
tasbihaanwaradil <tasbihaanwaradil@gmail.com>
2026-06-08 11:32:37 +0500

    Learned JavaScript basics Varaible and Operators

---
+++ Basics/Day-01/day1.js
@@ -0,0 +1,19 @@
+console.log("Tasbiha")
+console.log("JavaScript Learning")
+
+console.warn("This is warning")
+console.error("This is error")
+
+
+// Variables
+// Variables are use to store values or some data 
+
+
+// variable Declaration
+var a;
+
+// variable Initialization
+a = 12;
+
+// We can declare and initialize variable in a same line
+var a = 12;
