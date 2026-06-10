afa1d2bc0552e40627f4829c378bc5d210baf8bb
tasbihaanwaradil <tasbihaanwaradil@gmail.com>
2026-06-10 22:26:47 +0500

    Practiced Increment Decrement Operators Practice Questions

--- Basics/Day-01/day1.js
+++ Basics/Day-01/day1.js
@@ -1,72 +1,238 @@
-console.log("Tasbiha")
-console.log("JavaScript Learning")
+// console.log("Tasbiha")
+// console.log("JavaScript Learning")
 
-console.warn("This is warning")
-console.error("This is error")
-
-
-// Variables
-// Variables are use to store values or some data 
+// console.warn("This is warning")
+// console.error("This is error")
 
 
-// variable Declaration
-var a;
-
-// variable Initialization
-a = 12;
-
-// We can declare and initialize variable in a same line
-var a = 12;
-
-// Arithmetic Operators +,-,*,/,%
-
-var b = 12;
-console.log(a + b);  //24
-console.log(a - b);  //0
-console.log(a * b);  //144
-console.log(a / b);  //1
-console.log(a % b);  //0   12 % 12 = 0 Reminder
+// // Variables
+// // Variables are use to store values or some data 
 
 
-// ALERT
-// alert() displays a message in a pop-up box.
-// It is used to show information, warnings, or notifications to the user.
+// // variable Declaration
+// var a;
 
-alert("Hello! This is an Alert in JavaScript");
+// // variable Initialization
+// a = 12;
 
+// // We can declare and initialize variable in a same line
+// var a = 12;
 
-// CONFIRM
-// confirm() displays a confirmation dialog with "OK" and "Cancel" buttons.
-// It returns a Boolean value:
-// OK     → true
-// Cancel → false
+// // Arithmetic Operators +,-,*,/,%
 
-confirm("Is your age greater than 18?");
-
-
-// STORING THE RESULT OF CONFIRM
-// We can store the user's response in a variable
-// and use it to make decisions in our website/application.
-
-var ans = confirm("Is your age greater than 18?");
+// var b = 12;
+// console.log(a + b);  //24
+// console.log(a - b);  //0
+// console.log(a * b);  //144
+// console.log(a / b);  //1
+// console.log(a % b);  //0   12 % 12 = 0 Reminder
 
 
-// DISPLAYING THE RESULT
-// console.log() prints output in the browser console.
-// It will display:
-// true  → if the user clicks OK
-// false → if the user clicks Cancel
 
-console.log(ans);
+// // pre increment    ++a
+// // post increment   a++
+
+// // pre decrement    --a
+// // post decrement   a--
+
+// // let a = 5;
+// // console.log(a++);   //6
+// // console.log(++a)    //7
 
 
-// PROMPT
-// prompt() displays an input box and asks the user to enter a value.
+// // let a = 1;
 
-prompt("Enter your name?");
+// a++    //1
+// ++a    //2
+// --a    //1
+// a--    //0
+// --a    //-1
+// ++a    //0
+// ++a    //1
+// ++a    //2
+// --a    //1
+// ++a    //2
+// console.log(++a)  //3
+// console.log(a++)  //3
+// console.log(--a)  //3
 
-// Storing user input in a variable
-var prompt_output = prompt("Enter your name?");
 
-// Displaying the entered value in the console
-console.log(prompt_output);
+// // let a = 2;
+// // a-- // 1
+// // a++ // 2
+// // --a // 1
+// // ++a // 2
+// // --a // 1
+// // ++a // 2
+// // ++a // 3
+// // --a // 2
+// // a-- // 1
+// // a++ // 2
+// // console.log(a++) // 2
+// // console.log(--a) // 2
+// // console.log(a--) // 2
+
+// // let a = 10
+// // let b = 10
+
+// // console.log(a !== b);
+
+
+// // let b = 10
+
+// // let result = a+b;
+// // console.log(a+b)
+// // console.log(a-b)
+// // console.log(a*b)
+// // console.log(a/b)
+// // console.log(a%b)
+
+// // pre increment
+// // pre decrement
+// // post increment
+// // post decrement
+
+// // let a = 10
+// // // a++
+// // // console.log(++a) // 12
+
+// // console.log(a--)
+
+// // let a = 1;
+
+// // a++ // 2
+// // --a // 1
+// // --a // 0
+// // a-- // -1
+// // a++ // 0
+// // ++a // 1
+// // --a // 0
+// // a-- // -1
+// // ++a // 0
+// // a++ // 1
+// // console.log(++a) // 2
+// // console.log(--a) // 1
+// // console.log(a--) // 1
+
+// // let a = 9;
+// // let b = 9;
+
+// // == === != !== > < >= <=
+
+// // console.log(a <= b)
+
+// // greater than or equal || &&
+
+// // true !== false
+// // false !== true
+
+
+// // ALERT
+// // alert() displays a message in a pop-up box.
+// // It is used to show information, warnings, or notifications to the user.
+
+// alert("Hello! This is an Alert in JavaScript");
+
+
+// // CONFIRM
+// // confirm() displays a confirmation dialog with "OK" and "Cancel" buttons.
+// // It returns a Boolean value:
+// // OK     → true
+// // Cancel → false
+
+// confirm("Is your age greater than 18?");
+
+
+// // STORING THE RESULT OF CONFIRM
+// // We can store the user's response in a variable
+// // and use it to make decisions in our website/application.
+
+// var ans = confirm("Is your age greater than 18?");
+
+
+// // DISPLAYING THE RESULT
+// // console.log() prints output in the browser console.
+// // It will display:
+// // true  → if the user clicks OK
+// // false → if the user clicks Cancel
+
+// console.log(ans);
+
+
+// // PROMPT
+// // prompt() displays an input box and asks the user to enter a value.
+
+// prompt("Enter your name?");
+
+// // Storing user input in a variable
+// var prompt_output = prompt("Enter your name?");
+
+// // Displaying the entered value in the console
+// console.log(prompt_output);
+
+
+
+// //Practice Questions
+// // let a = 1;
+// // a++    //2
+// // ++a    //3
+// // --a    //2
+// // a--    //1
+// // --a    //0
+// // ++a    //1
+// // ++a    //2
+// // ++a    //3
+// // --a    //2
+// // ++a    //3
+// // console.log(++a) //4  
+// // console.log(a++) //4 
+// // console.log(--a) //4
+
+// // let a = 2;
+// // a--    //1
+// // a++    //2
+// // --a    //1
+// // ++a    //2
+// // --a    //1 
+// // ++a    //2
+// // ++a    //3
+// // --a    //2
+// // a--    //1
+// // a++    //2
+// // console.log(a++) //2
+// // console.log(--a) //2
+// // console.log(a--) //2
+
+
+// // let a = 1;
+
+// // a++ //2
+// // --a //1
+// // --a //0
+// // a-- //-1
+// // a++ //0
+// // ++a //1
+// // --a //0
+// // a-- //-1
+// // ++a //0
+// // a++ //1
+// // console.log(++a) //2
+// // console.log(--a) //1
+// // console.log(a--) //1
+
+
+
+
+// let a = 1;
+
+// console.log(++a) //2
+// console.log(a++) //2
+// console.log(a--) //2
+// console.log(a++) //2
+
+
+let a = 2;
+
+console.log(a++); //2
+console.log(++a); //4
+console.log(a--); //4
