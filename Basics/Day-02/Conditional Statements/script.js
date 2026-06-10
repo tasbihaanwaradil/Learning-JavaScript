6b2ca7e4eca74273562d5f1981225bef73a16f06
tasbihaanwaradil <tasbihaanwaradil@gmail.com>
2026-06-10 23:41:53 +0500

    Solved Question: Check number is positive or negative using conditional Statements

--- Basics/Day-02/Conditional Statements/script.js
+++ Basics/Day-02/Conditional Statements/script.js
@@ -1,16 +1,43 @@
 //Conditional Statements (Basic Introduction)
 
-if (10 < 5) {
-    console.log(true)
+// if (10 < 5) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+
+// var age = prompt("Enter your age: ")
+
+
+// if (age > 18) {
+//     console.log("You can vote")
+// } else {
+//     console.log("You can't vote")
+// }
+
+
+// let age = 60;                                                   //You are allowed to use LinkedIn
+//                                                                 //end
+// if (age >= 13 && age <= 60) {
+//     console.log('You are allowed to use LinkedIn')
+// } else {
+//     console.log('You are not allowed to use LinkedIn')
+// }
+
+// console.log('end')
+
+
+// Find Positive and Negative Number
+
+let num = 100;
+
+if (num < 0) {
+    console.log(num + " is a negative number")
+} else if (num === 0) {
+    console.log("Number is zero")
+
 } else {
-    console.log(false)
+    console.log(num + " is a positive number")
 }
 
-var age = prompt("Enter your age: ")
-
-
-if (age > 18) {
-    console.log("You can vote")
-} else {
-    console.log("You can't vote")
-}
+console.log('end of program')
