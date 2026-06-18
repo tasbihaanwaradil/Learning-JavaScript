ccfa6f5227fa9a31c956b8e61122622003ec2e2a
tasbihaanwaradil <tasbihaanwaradil@gmail.com>
2026-06-19 00:00:32 +0500

    Topic Covered: Strings

---
+++ Strings/script.js
@@ -0,0 +1,44 @@
+// let name = "Tasbiha"
+
+// console.log(name[0])
+// console.log(name[1])
+// console.log(name[2])
+// console.log(name[3])
+// console.log(name.length)
+
+// for (let i = 0; i < name.length; i++) {
+//     console.log(name[i])
+// }
+
+// console.log(name.charAt(0));
+// console.log(name.charCodeAt(1));
+
+// let first_name = "Tasbiha"
+// let last_name = "Anwar Adil"
+
+// let full_name = first_name + " " + last_name
+// console.log(full_name)
+
+//Recommended
+// console.log(`${first_name} ${last_name}`)
+
+// console.log(first_name.slice(0, 5))
+// console.log(first_name.toUpperCase())
+
+
+// let my_str = "          Hello          "
+// console.log(my_str)
+// console.log(my_str.trim())
+
+//replace
+
+// let new_str = "Hello World!"
+
+// console.log(new_str.replace("Hello","Heyy"));
+
+
+//Practice Question
+//javascript count vowels in words
+
+
+
