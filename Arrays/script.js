e328e5e8e820ee65b5675273b9698722f6bcad8e
tasbihaanwaradil <tasbihaanwaradil@gmail.com>
2026-06-21 13:35:40 +0500

    Topic Covered: Arrays in JavaScript

---
+++ Arrays/script.js
@@ -0,0 +1,35 @@
+//Arrays is Collection of data in JavaScript
+
+// const students = ["Tasbiha", 22, "1234", "BSCS", "2023"]
+// console.log(students)
+
+let marks = [70, 60, 80, 43, 22]
+// console.log(marks)
+
+//For Loop
+// for (let i = 0; i < marks.length; i++) {
+//     console.log(i, marks[i])
+// }
+
+//for of 
+// for (let mark of marks) {
+//     console.log(mark)
+// }
+
+// ====================
+// for of Loop
+// ====================
+
+// In a for of loop, you do not need to specify:
+// - Starting index
+// - Ending condition
+// - Increment/Decrement
+
+// The loop automatically starts from the first element
+// and continues until the last element of the iterable.
+
+//for in 
+// for (let index in marks) {
+//     console.log(index, marks[index])
+
+// }
