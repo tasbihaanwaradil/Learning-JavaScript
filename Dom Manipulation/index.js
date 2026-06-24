60868077c1bdb1387d9ead07298c2527d78584c9
tasbihaanwaradil <tasbihaanwaradil@gmail.com>
2026-06-24 23:35:27 +0500

    Topics Covered: id, class and query selectors and event listener in Dom Manipulation

--- Dom Manipulation/index.js
+++ Dom Manipulation/index.js
@@ -1,2 +1,34 @@
-const heading1 = document.getElementById("heading-1")
-heading1.innerText = "I am Learning DOM Manipulation in JavaScript"
+//select element by id
+// const heading1 = document.getElementById("heading-1")
+// heading1.innerText = "I am Learning DOM Manipulation in JavaScript"
+// heading1.style.color = "orange"
+// heading1.style.backgroundColor = "black"
+// heading1.style.fontSize= "50px"
+// console.log(heading1)
+
+
+//Select element by class name
+// const heading1 = document.getElementsByClassName("head-1")
+// // console.log(heading1)
+// heading1[0].style.color = "red"
+// heading1[1].style.color = "blue"
+
+//query selector
+// const heading = document.querySelector(".heading-1")
+// const red = document.querySelector(".red")
+// const green = document.querySelector(".green")
+
+// // console.log(heading, red, green)
+
+// function changeColorRed() {
+//     heading.innerHTML = "I am red"
+//     heading.style.color = "red"
+// }
+
+// //events
+// red.addEventListener("click", changeColorRed)
+// green.addEventListener("click", () => {
+//     heading.innerHTML = "I am green"
+//     heading.style.color = "green"
+// })
+
