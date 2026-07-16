b91f8534207ef4e22460b177ed3a71a3742336df
tasbihaanwaradil <tasbihaanwaradil@gmail.com>
2026-07-16 21:35:58 +0500

    Added getProducts() function and displayProducts() as a part of DOM project

--- Promises/script.js
+++ Promises/script.js
@@ -43,40 +43,122 @@
 
 //using async await
 //loading state
-const loadingState = document.getElementById("loading");
+// const loadingState = document.getElementById("loading");
 
-//products container
+// //products container
+// const productContainer = document.getElementById("products");
+
+// const getProducts = async () => {
+//   try {
+//     const response = await fetch("https://dummyjson.com/products");
+//     // console.log(response.json()) //return promise
+
+//     const products = await response.json();
+//     console.log(products.products);
+
+//     //remove loadingState
+//     loadingState.style.display = "none";
+
+//     //show products
+
+//     products.products.map((product) => {
+//       productContainer.innerHTML += `
+//             <div class="card">
+//              <img src="${product.images[0]}" alt="">
+//              <p>${product.category}</p>
+//              <h3>${product.title}</h3>
+//              <h2>$${product.price}</h2>
+//              <button>Add to Cart</button>
+//          </div>
+
+//           `;
+//     });
+//   } catch (error) {
+//     loadingState.innerHTML = `something went wrong`;
+//     console.log(error);
+//   }
+// };
+
+// getProducts(); //products will display
+
+//Project Async await
+
+// // Select Products Container
 const productContainer = document.getElementById("products");
 
-const getProducts = async () => {
+//loading state
+const loadingState = document.getElementById("loading");
+
+//Filters
+//Search products
+const searchInput = document.getElementById("search");
+
+//Search products by category
+const categorySelected = document.getElementById("category");
+
+//Search products by priceRange
+const priceRange = document.getElementById("priceRange");
+
+let allProducts = [];
+
+const categories = [
+  "men's clothing",
+  "jewelery",
+  "electronics",
+  "women's clothing",
+];
+
+//Inject Categories in HTML option tag
+categories.map((category) => {
+  categorySelected.innerHTML += `
+  <option value="all">${category}</option>
+  `;
+});
+
+//API call function using async await
+//API call return promise
+
+//Return Array of 20 Objects stored in allProducts empty array
+async function getProducts() {
   try {
-    const response = await fetch("https://fakestoreapi.com/products");
-    // console.log(response.json()) //return promise
+    const response = await fetch("https://fakestoreapi.com/products"); //return promise
+    // console.log(response);
 
     const products = await response.json();
-    // console.log(products)
+    // console.log(products);
 
-    //remove loadingState
+    //laoding state display none after display products
     loadingState.style.display = "none";
 
-    //show products
+    //allProducts empty array fill with products object we get from api call
+    allProducts = products;
+    console.log(allProducts); //It has 20 array of objects we will pass it to displayProducts function
 
-    products.map((product) => {
-      productContainer.innerHTML += `
-            <div class="card">
-             <img src="${product.image}" alt="">
-             <p>${product.category}</p>
-             <h3>${product.title}</h3>
-             <h2>$${product.price}</h2>
-             <button>Add to Cart</button>
-         </div>
-            
-          `;
-    });
+    //displayProducts function
+    displayProducts(allProducts);
   } catch (error) {
-    loadingState.innerHTML = `something went wrong`;
+    //loadingState
+    loadingState.innerHTML = "something went wrong";
     console.log(error);
   }
-};
+}
 
 getProducts();
+
+//displayProducts
+function displayProducts(products) {
+  productContainer.innerHTML = "";
+
+  products.map((product) => {
+    productContainer.innerHTML += `
+     <div class="card">
+            <img src="${product.image}" alt="">
+            <h3>${product.title}</h3>
+            <h2>Price : $${product.price}</h2>
+            <button>Add to Cart</button>
+        </div>`;
+  });
+}
+
+
+
